036ccdb14d192bca4b4928a24a47f17878d3357e
elijahnacc <elijah.newlon@g.austincc.edu>
2024-09-27 16:20:31 +0000

    Homework 4 Functions

--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,16 @@
 #
+
+def get_hour(epoch_seconds):
+    result = epoch_seconds // 3600
+    remainder = result % 24
+    return remainder
+
+def get_minutes(epoch_seconds):
+    result = epoch_seconds // 60
+    remainder = result % 60
+    return remainder
+
+def get_seconds(epoch_seconds):
+    result = epoch_seconds % 3600
+    remainder = result % 60
+    return remainder
